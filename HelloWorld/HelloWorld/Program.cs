8608efd5c91e80d0f4ccf02f0bae6fa05619945e
Julia Chen <JChen@softpro.corp>
2020-03-07 14:14:59 -0500

    rename

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -14,6 +14,16 @@ namespace HelloWorld
             Console.WriteLine(number3+1);
             number3 += 1;
             Console.WriteLine(number3);
+            Console.WriteLine("{0},{1}",byte.MinValue,byte.MaxValue);//0,255
+            Console.WriteLine("{0},{1}", float.MinValue, float.MaxValue);//0,255
+            byte l = 1;
+            int i = l;
+            string s = "1";
+            byte intToByte = (byte)i;
+            byte intToByte2 = Convert.ToByte(s);
+
+            int stringToInt = Convert.ToInt32(s);
+            int stringToInt2 = int.Parse(s);
         }
     }
 }
