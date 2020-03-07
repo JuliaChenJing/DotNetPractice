245c116567e23ba2f025e00284dcf68c82fe50ed
Julia Chen <JChen@softpro.corp>
2020-03-07 13:46:59 -0500

    overflow

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,13 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            const float Pi = 3.14f;
+            float number = 1.2f;
+            decimal number2 = 1.2m;
+            byte number3 = 255;
+            Console.WriteLine(number3+1);
+            number3 += 1;
+            Console.WriteLine(number3);
         }
     }
 }
