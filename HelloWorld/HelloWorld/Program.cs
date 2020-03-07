2fcfdea35342cb760d88b40e692effcae3f238c4
Julia Chen <JChen@softpro.corp>
2020-03-07 14:19:57 -0500

    data conversion and exception handling

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -8,7 +8,7 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World!");
             const float Pi = 3.14f;
-            float number = 1.2f;
+            float num = 1.2f;
             decimal number2 = 1.2m;
             byte number3 = 255;
             Console.WriteLine(number3+1);
@@ -21,9 +21,30 @@ namespace HelloWorld
             string s = "1";
             byte intToByte = (byte)i;
             byte intToByte2 = Convert.ToByte(s);
-
             int stringToInt = Convert.ToInt32(s);
             int stringToInt2 = int.Parse(s);
+
+            try
+            {
+                var number = "1234";
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("the number cannot be converted to byte");
+            }
+
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("the true cannot be converted to 1");
+            }
         }
     }
 }
