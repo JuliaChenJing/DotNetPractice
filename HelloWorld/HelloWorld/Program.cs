47edc5dd42e62b6d2a612fced03fba262a7fffd9
Julia Chen <JChen@softpro.corp>
2020-03-07 13:53:44 -0500

    updated.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            const float Pi = 3.14f;
+            float number = 1.2f;
+            decimal number2 = 1.2m;
+            byte number3 = 255;
+            Console.WriteLine(number3+1);
+            number3 += 1;
+            Console.WriteLine(number3);
+        }
+    }
+}
